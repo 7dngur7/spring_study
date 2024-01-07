7f1ebf1e41bf5962e943550fe70919811ff84dea
woohyuk <7dngur7@naver.com>
2024-01-07 13:44:28 +0900

    spring mvc 기초

--- 3.실습/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ 3.실습/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,12 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
+import hello.servlet.web.springmvc.v1.SpringMemberListControllerV1;
+import hello.servlet.web.springmvc.v1.SpringMemberSaveControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ServletComponentScan
